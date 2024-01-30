d55c1b6041bfbfc78748f85b79c7135bfafd6600
Amrendra Kumar <amrendra001122@gmail.com>
2024-01-30 09:11:19 +0530

    H

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
@@ -12,14 +13,14 @@ const port = process.env.PORT || 8000;
 
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
-const partials_path = path.join(__dirname, "../templates/partials");
+//..const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.static(static_path));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "hbs");
 app.set("views", template_path);
-hbs.registerPartials(partials_path);
+
 
 // ... Your routes here
 app.get("/", (req, res) =>{
@@ -92,6 +93,8 @@ app.post("/message", async (req, resp) => {
 app.listen(port, () => {
   console.log(`Server is running at port no ${port}`);
 });
+
+
 
 
 
